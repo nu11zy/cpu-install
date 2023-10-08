3acc722f86ab3b794e315e684ca73b330bc62342
nu11zy <55335158+nu11zy@users.noreply.github.com>
2023-10-08 05:57:18 +0300

    cleanup

--- users/nu11z/home.nix
+++ users/nu11z/home.nix
@@ -3,20 +3,14 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
+  # Home-manager version
+  home.stateVersion = "22.11";
+
+  # Home
   home.username = "nu11z";
   home.homeDirectory = "/home/nu11z";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "22.11"; # Please read the comment before changing.
-
+  # Software and configs
   # Browser
   programs.firefox = {
     enable = true;
@@ -38,9 +32,7 @@
     userName  = "nu11zy";
     userEmail = "55335158+nu11zy@users.noreply.github.com";
   };
-  
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
+
   home.packages = [
     # Terminal
     pkgs.alacritty
@@ -54,19 +46,6 @@
     pkgs.bitwarden
     # Mail client
     pkgs.thunderbird
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
   ];
 
   # Dotfiles
